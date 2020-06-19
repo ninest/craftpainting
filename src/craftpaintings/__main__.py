9cf074747f61d7159de2e20195b46a71109760f8
ninest <parth.kabra@gmail.com>
2020-06-20 02:26:03 +0800

    add argparse to create cli

--- src/craftpaintings/__main__.py
+++ src/craftpaintings/__main__.py
@@ -1,5 +1,13 @@
 from craftpaintings.painting import create_painting
-import os
+import argparse
 
-painting = create_painting('beach.png', size=25, gray=True)
+parser = argparse.ArgumentParser()
+parser.add_argument('imagepath', help='specify the input image path')
+parser.add_argument('--size', default=10, help='the painting output size')
+parser.add_argument('--gray', default=False, action='store_true', help='true/false depending on whether the output painting should be black and white')
+
+args = parser.parse_args()
+print(args)
+
+painting = create_painting(args.imagepath, size=int(args.size), gray=args.gray)
 painting.show()

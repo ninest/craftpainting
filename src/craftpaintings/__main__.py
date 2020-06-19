c66f8423c768699a36432e64ade46a53d6bbd420
ninest <parth.kabra@gmail.com>
2020-06-20 02:30:40 +0800

    add optional output path

--- src/craftpaintings/__main__.py
+++ src/craftpaintings/__main__.py
@@ -2,12 +2,16 @@ from craftpaintings.painting import create_painting
 import argparse
 
 parser = argparse.ArgumentParser()
-parser.add_argument('imagepath', help='specify the input image path')
-parser.add_argument('--size', default=10, help='the painting output size')
-parser.add_argument('--gray', default=False, action='store_true', help='true/false depending on whether the output painting should be black and white')
+parser.add_argument('imagepath', help='the input image path')
+parser.add_argument('-o', '--outputpath', help='the output image path')
+parser.add_argument('-s', '--size', default=10, help='the painting output size')
+parser.add_argument('-g', '--gray', default=False, action='store_true', help='true/false depending on whether the output painting should be black and white')
 
 args = parser.parse_args()
-print(args)
 
 painting = create_painting(args.imagepath, size=int(args.size), gray=args.gray)
-painting.show()
+
+if args.outputpath:
+  painting.save(args.outputpath)
+else:
+  painting.show()

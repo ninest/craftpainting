609828bca921cf56c20ebc66d7146c8d0fa2ccfa
ninest <parth.kabra@gmail.com>
2020-06-20 15:55:31 +0800

    add repetitions option to argparse

--- src/craftpainting/__main__.py
+++ src/craftpainting/__main__.py
@@ -1,16 +1,25 @@
 from craftpainting.painting import create_painting
+from craftpainting.enlarge import enlarge_image
 import argparse
 
 
 parser = argparse.ArgumentParser()
 parser.add_argument('imagepath', help='the input image path')
 parser.add_argument('-o', '--outputpath', help='the output image path')
-parser.add_argument('-s', '--size', default=16, help='the painting output size')
+parser.add_argument('-s', '--size', default='1x1', help='the painting output size (ex: 2x1)')
 parser.add_argument('-g', '--gray', default=False, action='store_true', help='whether the output painting should be black and white')
+parser.add_argument('-r', '--repeat', default=15, type=int, help='how many times multiply the pixels (set to 0 to get a Minecraft texture)')
 
 args = parser.parse_args()
 
-painting = create_painting(args.imagepath, size=int(args.size), gray=args.gray)
+# convert the size from AxB to (A, B)
+width = int(args.size.split('x')[0])
+height = int(args.size.split('x')[1])
+
+painting = create_painting(args.imagepath, size=(width, height), gray=args.gray)
+
+if args.repeat != 0:
+  painting = enlarge_image(painting, repetitions=args.repeat)
 
 if args.outputpath:
   painting.save(args.outputpath)
--- src/test.py
+++ src/test.py
@@ -5,14 +5,3 @@ from PIL import Image
 p = create_painting('images/cat.jpeg')
 p = enlarge_image(p, repetitions=20)
 p.show()
-# import numpy as np
-
-# a = np.array([
-#   [1,2,3],
-#   [4,5,6],
-#   [7,8,9],
-# ])
-
-# a = np.repeat(a, 10, axis=0)
-# a = np.repeat(a, 10, axis=1)
-# print(a)

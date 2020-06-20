fbbf8e1635952dcf62c943581dba92b99b700729
ninest <parth.kabra@gmail.com>
2020-06-20 13:04:22 +0800

    add external image

--- src/craftpainting/painting.py
+++ src/craftpainting/painting.py
@@ -1,6 +1,9 @@
 from PIL import Image
 import numpy as np
 import random
+import requests
+from io import BytesIO
+import validators
 
 BLOCK_SIZE = 16
 
@@ -11,8 +14,13 @@ FRAME_COLORS = [
 ]
 
 
-def create_painting(image_path, size=None, gray=False, border=False):
-  image = Image.open(image_path)
+def create_painting(image_path, size=None, gray=False, border=True):
+  # image path can be from internet too
+  if validators.url(image_path):
+    response = requests.get(image_path)
+    image = Image.open(BytesIO(response.content))
+  else:
+    image = Image.open(image_path)
 
   if size is None:
     # try to detect what the size should be if it's not provided
@@ -23,17 +31,17 @@ def create_painting(image_path, size=None, gray=False, border=False):
 
     if ratio > 1.33:
       size = (2, 1)
-    elif ratio < 0.75:
+    elif ratio < 0.76:
       size = (1, 2)
     else:
       size = (1, 1)
 
   # convert the size from block to pixels
-  size = size[0] * BLOCK_SIZE, size[1] * BLOCK_SIZE
+  image_size = size[0] * BLOCK_SIZE, size[1] * BLOCK_SIZE
 
   if border:
     # create a brown image
-    parent_image = Image.new('RGB', size)
+    parent_image = Image.new('RGB', image_size)
 
     array = np.array(parent_image)
     for row_index in range(len(array)):
@@ -43,7 +51,8 @@ def create_painting(image_path, size=None, gray=False, border=False):
     parent_image = Image.fromarray(array)
 
     # adjust the old size
-    image = image.resize((size[0]-2, size[1]-2), Image.ANTIALIAS)
+    # reduce it to make space for the frame
+    image = image.resize((image_size[0]-2, image_size[1]-2), Image.ANTIALIAS)
 
     # page the image on the parent_image so it has a border
     parent_image.paste(image, (1, 1))
@@ -52,7 +61,8 @@ def create_painting(image_path, size=None, gray=False, border=False):
     image = parent_image
 
   else:
-    image = image.resize(size, Image.ANTIALIAS)
+    # no border, just resize image
+    image = image.resize(image_size, Image.ANTIALIAS)
 
   # black and white images optional
   if gray:
--- src/test.py
+++ src/test.py
@@ -1,6 +1,5 @@
 from craftpainting.painting import create_painting
 
-# p = create_painting('sunset_dense.jpg', border=True)
-p = create_painting('beach.png', border=True)
+p = create_painting('https://live.staticflickr.com/967/28289693978_37b42a56a8_b.jpg')
 p.show()
 
